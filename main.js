2913139980a691796557a50d33fd514b3a8d2511
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-08-01 19:18:41 +0200

    add ability to input names for the players
    also add restart/start button that checks if user typed in both inputs,
    if so, invoke restartGame(name1,name2), else restartGame() without
    parameters

--- main.js
+++ main.js
@@ -8,12 +8,19 @@ const Gameboard = (() => {
   const placeMarker = (row, col, marker) => {
     board[row][col] = marker;
   };
-
+  const resetBoard = () => {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        board[i][j] = "";
+      }
+    }
+  }
   const getBoard = () => board;
 
   return {
     placeMarker,
     getBoard,
+    resetBoard
   };
 })();
 
@@ -21,12 +28,13 @@ const Gameboard = (() => {
 const Player = (name, marker) => {
   const getName = () => name;
   const getMarker = () => marker;
-  const placeMarker = (row, col) => Gameboard.placeMarker(row, col, marker);
-  return { getName, getMarker, placeMarker };
+  return { getName, getMarker };
 };
 
 const Gameflow = (player1, player2) => {
   let moveNumber = 0;
+  let gameFinished = false;
+  const getGameFinished = () => gameFinished;
 
   const players = [
     {
@@ -54,7 +62,11 @@ const Gameflow = (player1, player2) => {
       if (arr[0] === arr[1] && arr[1] == arr[2]) {
         if (arr[0] === "") continue;
         if (activePlayer.marker !== arr[0]) { switchPlayerTurn() }
-        return `Winner is ${activePlayer.marker} row=${i}`;
+        gameFinished = true;
+        return {
+          winner: activePlayer,
+          coords: [[i, 0], [i, 1], [i, 2]]
+        }
       }
 
       // three in a column
@@ -62,47 +74,76 @@ const Gameflow = (player1, player2) => {
       if (arr[0] === arr[1] && arr[1] == arr[2]) {
         if (arr[0] === "") continue;
         if (activePlayer.marker !== arr[0]) { switchPlayerTurn() }
-        return `Winner is ${activePlayer.marker} col=${i}`;
+        gameFinished = true;
+        return {
+          winner: activePlayer,
+          coords: [[0, i], [1][i], [2, i]]
+        }
       }
+
     }
 
     // three in diagonal
     if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
       if (board[0][0] === "") return "";
       if (activePlayer.marker !== board[0][0]) { switchPlayerTurn() }
-      return `Winner is ${activePlayer.marker} ${[[0, 0], [1, 1], [2, 2]]}`;
+      gameFinished = true;
+      return {
+        winner: activePlayer,
+        coords: [[0, 0], [1, 1], [2, 2]]
+      }
     }
     if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
       if (board[0][2] === "") return "";
       if (activePlayer.marker !== board[0][2]) { switchPlayerTurn() }
-      return `Winner is ${activePlayer.marker} ${[[0, 2], [1, 1], [2, 0]]}`;
+      gameFinished = true;
+      return {
+        winner: activePlayer,
+        coords: [[0, 2], [1, 1], [2, 0]]
+      }
     }
 
-    if (moveNumber === 9) return "tie";
+    if (moveNumber === 9) {
+      gameFinished = true;
+      return "tie";
+    }
 
     return "";
   };
 
   const getActivePlayer = () => activePlayer;
 
+  const restartGame = (player1Name, player2Name) => {
+    moveNumber = 0;
+    gameFinished = false;
+    activePlayer = players[0];
+    Gameboard.resetBoard();
+    if (player1Name !== "" && player2Name !== "") {
+      players[0].name = player1Name;
+      players[1].name = player2Name;
+    }
+  }
+
   const playRound = (row, column) => {
     moveNumber++;
     Gameboard.placeMarker(row, column, activePlayer.marker);
-
-    console.log(checkWinner());
-
     switchPlayerTurn();
   };
 
   return {
     getActivePlayer,
     playRound,
+    getGameFinished,
+    checkWinner,
+    restartGame,
   };
 };
 
 const DisplayController = (() => {
   const boardDiv = document.querySelector(".board");
-  const game = Gameflow(Player("bruh", "x"), Player("dawg", "o"));
+  const statusPara = document.querySelector(".status p");
+  const restartBtn = document.querySelector(".restart");
+  const game = Gameflow(Player("jeff", "x"), Player("dud", "o"));
 
   const updateScreen = () => {
     boardDiv.textContent = "";
@@ -119,20 +160,50 @@ const DisplayController = (() => {
         boardDiv.appendChild(p);
       }
     }
+
+    statusPara.textContent = `Current player: ${game.getActivePlayer().marker} (${game.getActivePlayer().name})`;
+
   };
 
-  const handleClick = (e) => {
+  const handleRestartClick = (e) => {
+    const player1Name = document.querySelector("#player1").value;
+    const player2Name = document.querySelector("#player2").value;
+    if (player1Name !== "" && player2Name !== "") {
+      game.restartGame(player1Name, player2Name);
+    } else {
+      game.restartGame();
+    }
+
+    updateScreen();
+  }
+
+
+  const handleBoardClick = (e) => {
     const row = e.target.dataset.row;
     const col = e.target.dataset.col;
 
     if (!row || !col) return;
     if (Gameboard.getBoard()[row][col] !== "") return;
+    if (game.getGameFinished()) return;
 
     game.playRound(row, col);
     updateScreen();
+
+    const check = game.checkWinner();
+    if (check === "tie") {
+      statusPara.textContent = "Tie!";
+      return;
+    }
+    if (check !== "") {
+      statusPara.textContent = `Winner = ${check.winner.marker} (${check.winner.name})`;
+      return;
+    }
+
   };
 
-  boardDiv.addEventListener("click", handleClick);
+  boardDiv.addEventListener("click", handleBoardClick);
+  restartBtn.addEventListener("click", handleRestartClick);
+
 
   updateScreen();
 })();
